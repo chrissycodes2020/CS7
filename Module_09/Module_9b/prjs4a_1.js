ee2cfe5ed9c3c77453a60db9676b8641f7fae99a
chrissycodes2020 <61220882+chrissycodes2020@users.noreply.github.com>
2021-11-07 15:09:27 -0800

    update

---
+++ Module_09/Module_9b/prjs4a_1.js
@@ -0,0 +1,20 @@
+function car_cost(mycar, paycheck) {
+
+window.alert('You have a ' + mycar + ' and make $' + paycheck)
+
+}
+
+car_cost('Mustang', 1500);
+
+function get_added_text() {
+    var textpart1 = 'This project ';
+    var textpart2 = 'is almost fun!';
+    var addedtext = textpart1 + textpart2;
+    return addedtext;
+}
+
+var alerttext = get_added_text();
+
+window.alert(alerttext);
+
+
